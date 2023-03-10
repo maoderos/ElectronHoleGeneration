a20965a1155b754950af8cc64d0eff83e7bab925
maoderos <marcos.deros@ufrgs.br>
2023-03-10 14:44:51 -0300

    Added macros

---
+++ run_all_Si.sh
@@ -0,0 +1,39 @@
+#MIP
+
+mkdir MIP_data;
+cd MIP_data;
+./ElectronHoleGeneration -m ../macros/Si_MIP.in -t 24;
+cd ..;
+mkdir MIP_data_NoMSC;
+cd MIP_data_NoMSC;
+./ElectronHoleGeneration -m ../macros/Si_MIP_NoMSC.in -t 24;
+cd ..;
+mkdir MIP_data_NoSEC;
+cd MIP_data_NoSEC;
+./ElectronHoleGeneration -m ../macros/Si_MIP_NoSEC.in -t 24;
+cd ..;
+mkdir MIP_data_NoMSCSEC;
+cd MIP_data_NoMSCSEC;
+./ElectronHoleGeneration -m ../macros/Si_MIP_NoMSCSEC.in -t 24;
+cd ..;
+
+#dE/dx
+mkdir dEdx_data;
+cd dEdx_data;
+./ElectronHoleGeneration -m ../macros/Si_dEdx.in -t 24;
+cd ..;
+mkdir dEdx_data_NoMSC;
+cd dEdx_data_NoMSC;
+./ElectronHoleGeneration -m ../macros/Si_dEdx_NoMSC.in -t 24;
+cd ..;
+mkdir dEdx_data_NoSEC;
+cd dEdx_data_NoSEC;
+./ElectronHoleGeneration -m ../macros/Si_dEdx_NoSEC.in -t 24;
+cd ..;
+mkdir dEdx_data_NoMSCSEC;
+cd dEdx_data_NoMSCSEC;
+./ElectronHoleGeneration -m ../macros/Si_dEdx_NoMSCSEC.in -t 24;
+cd ..;
+
+
+
